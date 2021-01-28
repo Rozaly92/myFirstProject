5d551fe1cf2876ceb5318c396a660ac7cb3d45a9
Rozaly92 <negrea.roza@mail.ru>
2021-01-28 14:20:55 +0200

    add applicationContext2.xml

--- myFirstProject/src/main/java/com/example/myFirstProject/spring_introduction/Dog.java
+++ myFirstProject/src/main/java/com/example/myFirstProject/spring_introduction/Dog.java
@@ -1,6 +1,7 @@
 package com.example.myFirstProject.spring_introduction;
 
 public class Dog implements Pet{
+  //  private String name;
     public Dog()
     {
         System.out.println("Dog's bean was created");
@@ -9,5 +10,20 @@ public class Dog implements Pet{
     public void say()
     {
         System.out.println("Bow-Bow");
+    }
+
+  /*  public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }*/
+
+    public void init(){
+        System.out.println("Class dog- init method");
+    }
+    public void destroy(){
+        System.out.println("Class dog- destroy method");
     }
 }
--- myFirstProject/src/main/java/com/example/myFirstProject/spring_introduction/Person.java
+++ myFirstProject/src/main/java/com/example/myFirstProject/spring_introduction/Person.java
@@ -2,13 +2,43 @@ package com.example.myFirstProject.spring_introduction;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
     public Person(Pet pet)
     {
-        System.out.println("Person bean is created!");
         this.pet=pet;
+    }
+    public Person()
+    {
+        System.out.println("Person bean is created!");
     }
     public void callYourPet(){
         System.out.println("Hello my lovely Pet!");
         pet.say();
+    }
+    public void setPet(Pet pet)
+    {
+        System.out.println("Class person: set Pet");
+        this.pet=pet;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
